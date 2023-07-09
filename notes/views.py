b49b03a76702ef266d17132a6710c81588bd8ad6
ChrishaneA <76893502+ChrisA2021@users.noreply.github.com>
2023-07-09 17:44:27 +1000

    Completed chapter 8

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,10 @@
+from typing import List
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NotesForm
 from .models import Notes
@@ -22,11 +25,21 @@ class NotesCreateView(CreateView):
     success_url = '/smart/notes'
     form_class = NotesForm
 
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url()) 
 
-class NotesListView(ListView):
+
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+    login_url = "/admin"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
     
 class NotesDetailView(DetailView):
     model = Notes
